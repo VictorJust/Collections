15f73f290bc85dd70dc1e707350611329d5f4284
VictorJust <victorjust09@gmail.com>
2023-05-20 15:54:12 +0300

    Add stack implementation

---
+++ Collections/Stack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Stack
+    {
+        List<int> list = new List<int>();
+        public void Push(int value)
+        {
+            list.Add(value);
+        }
+        public int Pop()
+        {
+            if (list.Count == 0) 
+                throw new InvalidOperationException();
+
+            var result = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return result;
+        }
+    }
+}
